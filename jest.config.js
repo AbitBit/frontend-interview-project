dd1c9a8421f4b09cfdf3facf155395cd269b8b94
beji <bejiabdelhamid1@gmail.com>
2020-11-15 15:31:40 +0100

    build(test): change infra to work with mdx stories

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',
+  transform: {
+    '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx',
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
   testEnvironment: 'node',
   moduleNameMapper: {
     '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
