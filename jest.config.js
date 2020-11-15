cf4949efa911286966fc5d9ec41c5935ee0d83ea
beji <bejiabdelhamid1@gmail.com>
2020-11-15 17:45:02 +0100

    fix(test): revert code due to SB mdx bug

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
     '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx',
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
-  testEnvironment: 'node',
   moduleNameMapper: {
     '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
   },
